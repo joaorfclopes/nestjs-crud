909eb6324b0cd47b27b39363a3c848934e8b865f
joaorfclopes <joaorfclopes@gmail.com>
2021-10-29 19:53:03 +0100

    main server endpoint updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getMain(): any {
+    return {
+      name: 'notes-api',
+      version: '0.0.1',
+      author: 'joaorfclopes',
+    };
   }
 }
